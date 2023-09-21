dae40c13388e752585752a008c259b90d94e8bc0
HAlbergson <hjalte3480@gmail.com>
2023-09-21 09:10:39 +0200

    fjernet add track, og lavet close dialog function som lukker dialog of fjerner tilføjede linjer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function initApp() {
   document.querySelector("#add-album-button").addEventListener("click", openAlbumDialog);
   document.querySelector("#add-track-button-album").addEventListener("click", addTrackInputField);
   document.querySelector("#add-artist-button-album").addEventListener("click", addArtistInputField);
+  document.querySelector("#close-album-dialog").addEventListener("click", closeAlbumDialog);
 }
 
 function openAlbumDialog() {
@@ -26,4 +27,34 @@ function addArtistInputField() {
   newArtistInput.className = "artist-input";
   newArtistInput.name = "artist-name";
   artistList.appendChild(newArtistInput);
+}
+function closeAlbumDialog() {
+  const albumDialog = document.querySelector("#album-dialog");
+  const albumTitleInput = document.querySelector("#album-title");
+  const artistInputs = document.querySelectorAll(".artist-input");
+  const trackInputs = document.querySelectorAll(".track-input");
+
+  // Nulstil inputfelter
+  albumTitleInput.value = "";
+  artistInputs.forEach(function (input) {
+    input.value = "";
+  });
+  trackInputs.forEach(function (input) {
+    input.value = "";
+  });
+
+  // Fjern de dynamisk tilføjede inputfelter, men bevar label-elementerne
+  const dynamicTrackInputs = document.querySelectorAll(".track-input:not(:first-child)");
+  dynamicTrackInputs.forEach(function (input) {
+    input.parentNode.removeChild(input);
+  });
+
+  const dynamicArtistInputs = document.querySelectorAll(".artist-input:not(:first-child)");
+  dynamicArtistInputs.forEach(function (input) {
+    input.parentNode.removeChild(input);
+  });
+  addArtistInputField();
+  addTrackInputField();
+
+  albumDialog.close();
 }
